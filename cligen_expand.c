3c6dca0d504d25722d1c181c862f11254b2d5f72
Olof Hagsand <olof@hagsand.se>
2013-09-20 13:05:01 +0200

    better warning

--- cligen_expand.c
+++ cligen_expand.c
@@ -252,8 +252,8 @@ pt_expand_1(cligen_handle h, cg_obj *coprev, parse_tree *pt)
 
 		/* Find the referring tree */
 		if ((ptt0 = cligen_tree(h, treeref)) == NULL){
-		    fprintf(stderr, "%s: CLIgen subtree %s not found\n", 
-			    __FUNCTION__, treeref);
+		    fprintf(stderr, "CLIgen subtree %s not found\n", 
+			    treeref);
 		    return -1;
 		}
 
