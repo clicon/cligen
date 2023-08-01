cc540a4087cc5884d7d8a5b224f8ec759bb047ba
Olof hagsand <olof@hagsand.se>
2023-08-01 10:55:25 +0200

    Removed obsolete functions `pt_name_get()` / `pt_name_set()`

--- cligen_parsetree.h
+++ cligen_parsetree.h
@@ -72,8 +72,6 @@ int         pt_vec_i_insert(parse_tree *pt, int i, cg_obj *co);
 int         pt_vec_append(parse_tree *pt, cg_obj *co);
 int         pt_vec_i_delete(parse_tree *pt, int i, int recurse);
 int         pt_len_get(parse_tree *pt);
-char       *pt_name_get(parse_tree *pt);
-int         pt_name_set(parse_tree *pt, char *name);
 int         pt_sets_get(parse_tree *pt);
 int         pt_sets_set(parse_tree *pt, int sets);
 void        cligen_parsetree_sort(parse_tree *pt, int recursive);
